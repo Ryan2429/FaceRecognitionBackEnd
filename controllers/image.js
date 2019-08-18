223a3bd1b93de3b3850988d90e5ffff71ce28c6b
Ryan2429 <aldous.ryan1@gmail.com>
2019-08-18 18:40:57 -0400

    refactored and updated security

---
+++ controllers/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: 'fa27d17a53d14f7f84ee0c1457b1924f'
+});
+
+ const handleApiCall = (req, res) => {
+     app.models
+     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+     .then(data => {
+         res.json(data);
+     })
+     .catch(err => res.status(400).json('unable to work with API'))
+ }
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('Not found'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
